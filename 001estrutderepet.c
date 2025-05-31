758d0e81c576652f51ee34a6b9d416b344ce6f69
RicardoOliveira2025 <202503392692@alunos.estacio.br>
2025-05-31 01:14:53 +0000

    parouimparwhile

--- 001estrutderepet.c
+++ 001estrutderepet.c
@@ -6,16 +6,18 @@ int main(){
 
     do {
 
-        printf("Digite um numero par para sair do programa...");
+        printf("Digite um numero par para sair do programa...\n");
         scanf("%d", &numero);
 
         if(numero % 2 == 0){
-            printf("%d eh par", numero);
+            printf("%d eh par \n", numero);
         } else {
-            printf("%d eh impar", numero);
+            printf("%d eh impar \n", numero);
         }
 
     } while (numero % 2 != 0);
+
+    printf("Voce digitou um numero par, saindo do programa...\n");
 
     return 0;
 }
